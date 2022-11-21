b16a9f930ae8b20c8deb893ceb273a59fe2ca037
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-21 19:05:23 +0000

    Sign up confirmation functionality and update layout

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 6.0.6'
 gem "devise"
 gem "twitter-bootstrap-rails"
 gem "devise-bootstrap-views"
+gem "font-awesome-rails"
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 465,
+  domain: 'gmail.com',
+  user_name: Rails.application.credentials.gmail.fetch(:user_name),
+  password: Rails.application.credentials.gmail.fetch(:password),
+  authentication: 'plain',
+  ssl: true,
+  tsl: true,
+  enable_starttls_auto: true
+}
